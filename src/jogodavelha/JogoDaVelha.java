04726822632224a2825b67c7d32c9c96c56a1dbd
ti01 <ti01@192.168.1.174>
2014-11-12 16:22:57 -0200

    Inicio

---
+++ src/jogodavelha/JogoDaVelha.java
@@ -0,0 +1,12 @@
+package jogodavelha;
+
+public class JogoDaVelha {
+
+    public static void main(String[] args) {
+        //Tela tela = new Tela();
+        //tela.show();
+        MiniMax miniMax = new MiniMax();
+        miniMax.teste();
+    }
+    
+}
---
+++ src/jogodavelha/Regras.java
@@ -0,0 +1,13 @@
+package jogodavelha;
+
+public interface Regras {
+
+    public char[] verificarJogada(char tabuleiro[]);
+    public char[] novoJogo(char tabuleiro[]);
+    public boolean verificarSePerdeu(char tabuleiro[]);
+    public char[] verificarSePodeGanhar(char tabuleiro[]);
+    public char[] bloquearJogador(char tabuleiro[]);
+    public char[] escolherPosicaoComChanceDeGanhar(char tabuleiro[]);
+    public char[] bloquearPosicaoComChanceDePerto(char tabuleiro[]);
+    public char[] escolherMelhorPosicao(char tabuleiro[]);
+}
