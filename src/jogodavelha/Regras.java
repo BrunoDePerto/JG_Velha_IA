761d1caff23397b489dedd3add856ef33b2ce35e
ti01 <ti01@192.168.1.174>
2014-11-14 17:31:17 -0200

    Criando a logica

--- src/jogodavelha/Regras.java
+++ src/jogodavelha/Regras.java
@@ -4,11 +4,11 @@ public interface Regras {
 
     public char[] efetuarJogada(char tabuleiro[]);
     public char[] novoJogo(char tabuleiro[]);
-    public boolean verificarSePerdeu(char tabuleiro[]);
-    public char[] verificarSePodeGanhar(char tabuleiro[]);
-    public char[] bloquearJogador(char tabuleiro[]);
-    public char[] escolherPosicaoComChanceDeGanhar(char tabuleiro[]);
-    public char[] bloquearPosicaoComChanceDePerto(char tabuleiro[]);
+    public boolean verificarGanhador(char tabuleiro[], char jogador);
+    public boolean verificarSePodeGanhar(char tabuleiro[], int[][] melhoresEscolhas);
+    public boolean bloquearJogador(char tabuleiro[], int[][] melhoresEscolhas);
+    public boolean escolherPosicaoComChanceDeGanhar(char tabuleiro[]);
+    public boolean bloquearPosicaoComChanceDePerto(char tabuleiro[]);
     public char[] escolherMelhorPosicao(char tabuleiro[]);
     
 }
